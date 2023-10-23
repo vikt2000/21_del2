741079b1ef843efc1350bb50bb687fcebef86155
jonaswoetmann <144006671+jonaswoetmann@users.noreply.github.com>
2023-10-23 15:37:05 +0200

    added dice funciton

--- Game.java
+++ Game.java
@@ -4,6 +4,35 @@ class Spiller {
     public Spiller(String name) {
         this.name = name;
     }
+
+    private int d1;
+    private int d2;
+    private int sum;
+
+    public DiceRoll() {
+        sum = 0;
+    }
+
+    public void rollDice() {
+        Random ran = new Random();
+
+        d1 = ran.nextInt(6) + 1;
+        d2 = ran.nextInt(6) + 1;
+        sum = d1 + d2;
+        
+    }
+
+    public int getd1() {
+        return d1;
+    }
+
+    public int getd2() {
+        return d2;
+    }
+
+    public int getSum() {
+        return sum;
+    }
 }
 
 class Konto {
