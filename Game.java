28f1929fe8ebb5387be75b54ae3f67198ff27d41
antonhtmlito <antondong04@gmail.com>
2023-10-24 21:24:56 +0200

    Alt tekst er sat under class TextString

--- Game.java
+++ Game.java
@@ -23,7 +23,10 @@ class TextString {
     final int ENGLISH = 0;
     final int DANISH = 1;
     public static String[] felter;
-
+    public static String BALANCE_TEXT;
+    public static String WIN_TEXT;
+    public static String ENTER_FIRST_PLAYER_NAME;
+    public static String ENTER_SECOND_PLAYER_NAME;
     public TextString() {
         new TextString(ENGLISH);
     }
@@ -33,15 +36,41 @@ class TextString {
      */
     public TextString(int language) {
         if (language == ENGLISH) {
-            felter = new String[] { "", "", "Tower", "Crater", "Palace gates", "Cold Desert",
-                    "Walled city", "Monastery", "Black cave", "Huts in the mountain",
-                    "The Werewall (werewolf-wall)", "The pit", "Goldmine"
+            felter = new String[] { "", "",
+                    " has arrived at a Tower and found silver worth 250.",
+                    " has arrived at a Crater and spent 100 on rope.",
+                    " has arrived at the Palace gates and received 100 from a generous passerby.",
+                    " has arrived at a Cold Desert and spent 20 on a hot coat",
+                    " has arrived at Walled city and received 180 from the mayor.",
+                    " has arrived at the Monastery and stays one night for free.",
+                    " has arrived at a Black cave and spent 70 on torches.",
+                    " has arrived at some Huts in the mountain and found spare wood worth 60.",
+                    " has arrived at The Werewall (werewolf-wall) and spent 80 on meat, so he could bribe them",
+                    " has arrived at The pit and spent 50 on a ladder to escape",
+                    " has arrived at Goldmine and found gold worth 650, he's rich!"
             };
+            BALANCE_TEXT = " balance in total : ";
+            WIN_TEXT =" has balance over 3000 and won the game";
+            ENTER_FIRST_PLAYER_NAME = "Enter the first player's name : ";
+            ENTER_SECOND_PLAYER_NAME = "Enter the second player's name : ";
         } else if (language == DANISH) {
-            felter = new String[] { "", "", "Tower", "Crater", "Palace gates", "Cold Desert",
-                    "Walled city", "Monastery", "Black cave", "Huts in the mountain",
-                    "The Werewall (werewolf-wall)", "The pit", "Goldmine"
+            felter = new String[] { "", "",
+                    " er ankommet til en tårn og har fundet sølv til en værdi af 250.",
+                    " er ankommet til en krater og har brugt 100 på reb.",
+                    " er ankommet til paladsgitteret og har modtaget 100 fra en gavmild forbipasserende.",
+                    " er ankommet til en kold ørken og har brugt 20 på en varm frakke.",
+                    " er ankommet til en befæstet by og har modtaget 180 fra borgmesteren.",
+                    " er ankommet til et kloster og overnatter gratis.",
+                    " er ankommet til en sort hule og har brugt 70 på fakler.",
+                    " er ankommet til nogle hytter i bjergene og har fundet reservedele af træ til en værdi af 60.",
+                    " er ankommet til Ulvevæggen og har brugt 80 på kød for at kunne bestikke dem.",
+                    " er ankommet til afgrunden og har brugt 50 på en stige for at undslippe.",
+                    " er ankommet til en guldmine og har fundet guld til en værdi af 650. Han er rig!"
             };
+            BALANCE_TEXT = " balance in total : ";
+            WIN_TEXT =" has a balance over 3000 and won the game.";
+            ENTER_FIRST_PLAYER_NAME = "Enter the first player's name : ";
+            ENTER_SECOND_PLAYER_NAME = "Enter the second player's name : ";
         }
 
     }
@@ -56,15 +85,7 @@ class Spiller {
     private Konto ownKonto;
 
     public String toString() {
-        if (Values.values[sum] == 0)
-            return "Player " + name + " has arrived at " + TextString.felter[sum]
-                    + " where they stay for an evening for free.";
-        else if (Values.values[sum] > 0)
-            return "Player " + name + " has arrived at " + TextString.felter[sum] + " and received "
-                    + Values.values[sum] + ".";
-        else
-            return "Player " + name + " has arrived at " + TextString.felter[sum] + " and has paid "
-                    + Values.values[sum] + " to escape.";
+        return name + TextString.felter[sum];
     }
 
     public void setKonto(Konto konto) {
@@ -76,7 +97,7 @@ class Spiller {
     }
 
     public void getBalance() {
-        System.out.println("Player " + name + " has got " + ownKonto.getBalance());
+        System.out.println(name + "'s" + TextString.BALANCE_TEXT + ownKonto.getBalance() + ".");
     }
 
     public Spiller(String name) {
@@ -143,9 +164,9 @@ class Konto {
 
     public String toString() {
         if (checkWinner())
-            return "The balance has exceeded 3000, " + spiller.getName() + " won!";
+            return spiller.getName() + " : " + TextString.WIN_TEXT;
         else
-            return "The balance for " + spiller.getName() + " is " + getBalance();
+            return spiller.getName() + " : " + TextString.BALANCE_TEXT + getBalance();
     }
 }
 
@@ -154,12 +175,12 @@ class PlayGame {
     public static void main(String[] argv) {
         TextString textString = new TextString();
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter first player name: ");
+        System.out.print(TextString.ENTER_FIRST_PLAYER_NAME);
         String fname = input.nextLine();
         Spiller p1 = new Spiller(fname);
         Konto k1 = new Konto(1000, p1);
         p1.setKonto(k1);
-        System.out.print("Enter second player name: ");
+        System.out.print(TextString.ENTER_SECOND_PLAYER_NAME);
         String sname = input.nextLine();
         Spiller p2 = new Spiller(sname);
         Konto k2 = new Konto(1000, p2);
